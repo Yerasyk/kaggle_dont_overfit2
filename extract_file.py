f0713cf8f447639e16dcfb4c5825d24702928de0
Yerassyl <144085980+Yerasyk@users.noreply.github.com>
2026-03-30 19:24:13 +0800

    Delete extract_file.py

--- extract_file.py
+++
@@ -1,6 +0,0 @@
-import zipfile
-
-with zipfile.ZipFile('data/old.zip', 'r') as zip_ref:
-    zip_ref.extractall('data/')
-
-# kaggle competitions submit -c dont-overfit-ii -f knn_submission.csv -m "Message"
